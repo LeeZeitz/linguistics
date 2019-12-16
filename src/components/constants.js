19e123e504821d2fbb5e3a967e4bfde7070d5bd0
Kate Harder <57335151+klharder@users.noreply.github.com>
2019-12-15 17:38:34 -0800

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -59,10 +59,10 @@ const stimLabels = {
 
 const trialVars = [
     {
-        'audio': 'gameover.mp3'
+        'audio': 'HiM_Crk_1_Norm.mp3'
     },
     {
-        'audio': 'gameover.mp3'
+        'audio': 'HiM_Crk_2_Norm.mp3'
     }
 ]
 
